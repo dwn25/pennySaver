ec78536b43a12c15c7307150deb1443dbd3fd057
David William Nartey <nartey.dw@gmail.com>
2019-08-09 11:54:23 -0400

    First Push

---
+++ pennySaver/src/com/Constant.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ *
+ * @author DNartey
+ */
+public class Constant {
+    
+    public static String currentUser = "";
+    public static String currentUserID = "";
+    public static int IDS = 4;
+}
